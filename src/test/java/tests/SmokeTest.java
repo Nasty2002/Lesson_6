81047a3c8f14f3c78d45c272ccadc8b3892146f4
Nastassia Kazeka <nastyakozeka@yandex.by>
2022-01-12 20:17:45 +0300

    New Test

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -2,26 +2,39 @@ package tests;
 
 import baseEntities.BaseTest;
 import core.ReadProperties;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.LoginPage;
+import untils.Retry;
 
 public class SmokeTest extends BaseTest {
 
     @Test
-    public void loginTest1(){
-        LoginPage loginPage=new LoginPage(driver);
+    public void loginTest() {
+        LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.getEmailField().sendKeys(ReadProperties.getPassword());
+        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
         loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
         loginPage.getLoginButton().click();
 
         DashboardPage dashboardPage = new DashboardPage(driver);
-        driver.get("");
+        waits.waitForVisibility(dashboardPage.getAddProjectButton());
 
+        Assert.assertTrue(dashboardPage.isPageOpened());
+    }
 
-        dashboardPage = new DashboardPage(driver, true)
+    @Test(retryAnalyzer = Retry.class)
+    public void flakyLoginTest() {
+        LoginPage loginPage = new LoginPage(driver);
 
+        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
+        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
+        loginPage.getLoginButton().click();
 
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        waits.waitForVisibility(dashboardPage.getAddProjectButton());
+
+        Assert.assertTrue(dashboardPage.isPageOpened());
     }
 }
