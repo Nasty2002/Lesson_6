17fa6001bd1438c51852dcb005be40ee0aed4212
Nastassia Kazeka <nastyakozeka@yandex.by>
2022-01-17 22:15:07 +0300

    New Test

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,27 +1,15 @@
 package models;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class User {
     private String email;
     private String password;
-
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -2,15 +2,9 @@ package tests;
 
 import baseEntities.BaseTest;
 import core.ReadProperties;
-import enums.ProjectType;
 import models.Project;
-import models.UserBuilder;
-import org.testng.Assert;
+import models.User;
 import org.testng.annotations.Test;
-import pages.DashboardPage;
-import pages.LoginPage;
-import untils.Randomization;
-import untils.Retry;
 
 public class SmokeTest extends BaseTest {
     Project addProject;
@@ -18,62 +12,14 @@ public class SmokeTest extends BaseTest {
 
     @Test
     public void loginTest() {
-        LoginPage loginPage = new LoginPage(driver);
+        User user = new User();
+        user.setEmail(ReadProperties.getUsername());
+        user.setPassword(ReadProperties.getPassword());
 
-        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
-        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
-        loginPage.getLoginButton().click();
+        User user1 = new User();
+        user1.setEmail(ReadProperties.getUsername());
+        user1.setPassword(ReadProperties.getPassword());
 
-        DashboardPage dashboardPage = new DashboardPage(driver);
-        driver.get("https://qa1504.testrail.io/index.php?/admin/overview");
-
-        dashboardPage = new DashboardPage(driver, true);
-        Assert.assertTrue(dashboardPage.getAddProjectButton().isDisplayed());
-    }
-
-    @Test
-    public void loginTestWithBuilder() {
-        UserBuilder user = new UserBuilder.Builder()
-                .withEmail(ReadProperties.getUsername())
-                .withPassword(ReadProperties.getPassword())
-                .build();
-
-        LoginPage loginPage = new LoginPage(driver);
-        //loginPage.login(user);
-
-        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
-        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
-        loginPage.getLoginButton().click();
-
-        DashboardPage dashboardPage = new DashboardPage(driver);
-        driver.get("https://qa1504.testrail.io/index.php?/admin/overview");
-
-        dashboardPage = new DashboardPage(driver, true);
-        Assert.assertTrue(dashboardPage.getAddProjectButton().isDisplayed());
-    }
-
-    @Test (retryAnalyzer = Retry.class)
-    public void flakyLoginTest() {
-        LoginPage loginPage = new LoginPage(driver);
-
-        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
-        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
-        loginPage.getLoginButton().click();
-
-        DashboardPage dashboardPage = new DashboardPage(driver);
-        driver.get("https://qa1504.testrail.io/index.php?/admin/overview");
-
-        dashboardPage = new DashboardPage(driver, true);
-        Assert.assertTrue(dashboardPage.getAddProjectButton().isDisplayed());
-
-    }
-    private void setUpProjects(){
-        addProject = new Project();
-        addProject.setName(Randomization.getRandomString(8));
-        addProject.setTypeOfProject(ProjectType.MULTIPLE);
-
-        updateProject = new Project();
-        updateProject.setName(Randomization.getRandomString(8));
-        updateProject.setTypeOfProject(Randomization.getRandomType(2));
+        System.out.println(toString());
     }
 }
