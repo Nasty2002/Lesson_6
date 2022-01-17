c2981a5e92c4c36a534a7490e4b732dcca3a199b
Nastassia Kazeka <nastyakozeka@yandex.by>
2022-01-17 21:53:03 +0300

    New Test

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -4,6 +4,7 @@ import baseEntities.BaseTest;
 import core.ReadProperties;
 import enums.ProjectType;
 import models.Project;
+import models.UserBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
@@ -18,6 +19,27 @@ public class SmokeTest extends BaseTest {
     @Test
     public void loginTest() {
         LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
+        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
+        loginPage.getLoginButton().click();
+
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        driver.get("https://qa1504.testrail.io/index.php?/admin/overview");
+
+        dashboardPage = new DashboardPage(driver, true);
+        Assert.assertTrue(dashboardPage.getAddProjectButton().isDisplayed());
+    }
+
+    @Test
+    public void loginTestWithBuilder() {
+        UserBuilder user = new UserBuilder.Builder()
+                .withEmail(ReadProperties.getUsername())
+                .withPassword(ReadProperties.getPassword())
+                .build();
+
+        LoginPage loginPage = new LoginPage(driver);
+        //loginPage.login(user);
 
         loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
         loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
